8f2422fbb3475a311e3ffeced6a6f7ff306a7792
Jack Zhang <jzhangkun1986@gmail.com>
2017-11-13 10:17:44 +0800

    used for analytics of Report Suite Data.tab

---
+++ omniture-migration/omniture_custom_vars.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+my %prop;  # Traffic Property
+my %evar;  # Conversion Variable
+my %event; # Event
+while (<>) {
+  #print;
+  parse_prop(\%prop, $') if m{^Traffic Property\s+};
+  parse_evar(\%evar, $') if m{^Conversion Variable\s+};
+  parse_event(\%event,$') if m{^Event\s+};
+}
+
+print Dumper \%prop;
+print Dumper \%evar;
+print Dumper \%event;
+
+output_std('prop', \%prop);
+output_std('eVar', \%evar);
+output_std('custom event', $event{Custom}) if exists $event{Custom};
+output_std('standard event', $event{Standard}) if exists $event{Standard};
+
+exit 0;
+
+sub output_std {
+  my ($prefix, $rh) = @_;
+  for my $k (sort { $a <=> $b } keys %$rh) {
+    print "${prefix}$k = ", $rh->{$k}{Name}. "\n";
+  }
+}
+
+sub parse_prop {
+  my ($rh, $line) = @_;
+  if ($line =~ m{^Traffic Property (\d+): (.+)}) {
+    my ($id, $attr) = ($1, $2);
+    my @attrs = split /, / => $attr;
+    my $name = (split /=/, $attrs[0])[1];
+    my $status = $attrs[1];
+    $rh->{$id} = {
+      Name => $name,
+      Status => $status,
+    };
+  }
+}
+
+sub parse_evar {
+  my ($rh, $line) = @_;
+  #print $line;
+  if ($line =~ m{^Conversion Variable (\d+)[.] Status : (Enabled|Disabled) Name : (.+?) Type : .+}) {
+    my ($id, $status, $name) = ($1, $2, $3);
+    return if $status eq 'Disabled';
+    $rh->{$id} = {
+      Name => $name,
+      Status => $status
+    };
+  }
+}
+
+sub parse_event {
+  my ($rh, $line) = @_;
+  #print $line;
+  if ($line =~ m{^(Custom|Standard) Event (\d+): (.+)}) {
+    my ($event, $id, $attr) = ($1, $2, $3);
+    my %attr;
+    # Name, Type etc...
+    for my $field (split /: / => $attr) {
+      my ($k, $v) = split /=/, $field;
+      $attr{$k} = $v;
+    }
+    # skip the event name with "Custom", they're not in use probably
+    return if $attr{Name} =~ m{^Custom \d+};
+    $rh->{$event}{$id} = \%attr;
+  }
+}
